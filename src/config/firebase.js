d72d73be6b2efec98c0e31c04eee3c67ad548097
jokosuntoro <jokosunt@gmail.com>
2020-10-17 05:28:56 +0700

    tambah routing dan firebase context

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCSMNo193Jq_m4zwzrppcYmffMmgny8RMg",
+    authDomain: "aplikasi-penjualan-jago.firebaseapp.com",
+    databaseURL: "https://aplikasi-penjualan-jago.firebaseio.com",
+    projectId: "aplikasi-penjualan-jago",
+    storageBucket: "aplikasi-penjualan-jago.appspot.com",
+    messagingSenderId: "869084486100",
+    appId: "1:869084486100:web:1002abfcc1bb3fe5ab9e41",
+    measurementId: "G-C5G5KW8FVB"
+  };
+
+  export default firebaseConfig;
---
+++ src/pages/private/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {Switch, Route} from 'react-router-dom';
+import Home from './home';
+
+//import komponen halaman private
+import Pengaturan from './pengaturan';
+import Produk from './produk';
+import Transaksi from './transaksi';
+
+function Private() {
+
+    return (
+        <Switch>
+            <Route path="/pengaturan" component={Pengaturan} />
+            <Route path="/produk" component={Produk} />
+            <Route path="/transaksi" component={Transaksi} />
+            <Route component={Home} />
+        </Switch>
+    )
+}
+
+export default Private;
